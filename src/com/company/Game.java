4547cbc20e013239a46e3c654e900a8dfa8e13f0
snabelA00 <ja000411@gmail.com>
2020-10-15 23:41:56 +0200

    Created start of Game.
    - created list of players
    - created main method which runs through the number of rounds
    - Figured out a way to initialize the chosen animal with name and gender.

--- src/com/company/Game.java
+++ src/com/company/Game.java
@@ -12,7 +12,6 @@ public class Game {
         print("\n".repeat(1));
         print("F A R M I N G  S I M U L A T O R  -  2 0 2 0 \n(1 - 4 players)");
         //Start game
-        //Print line
         // how many rounds?
         //how many players?
         print("\n".repeat(1));
@@ -36,8 +35,18 @@ public class Game {
 
 
     public void main(int numberOfRounds){
+
         for(int i = 1; i <= numberOfRounds; i++){
-            print("- Lets begin!\n" + "");
+            print("- Lets begin! What's your next move? (write a number)");
+            print(" 1. Buy an Animal\n 2. Sell an Animal\n 3. Buy Food\n 4. Feed animals");
+            int action = Integer.valueOf(scanner.nextLine());
+            switch (action){
+                case 1 -> new Store().buyAnimals();
+                case 2 -> new Store().sellAnimals();
+                case 3 -> new Store().buyFood();
+                case 4 -> new Animal("horsy", "female").feedAnimals();
+            }
+            print("End of turn");
         }
 
     }
