ec5a82556fc21edb09348cd43dd1452980fecd47
snabelA00 <ja000411@gmail.com>
2020-10-19 18:48:42 +0200

    Animal constructor - Initializes Animal objects with name, gender and health.
    added getters

--- src/com/company/Animal.java
+++ src/com/company/Animal.java
@@ -4,15 +4,31 @@ public class Animal {
 
     protected String name;
     protected String gender;
-    protected int hp = 100;
+    protected int health;
 
-    public Animal(String name, String gender){
+
+    public Animal(String name, String gender, int healthPoints){
         this.name = name;
         this.gender = gender;
+        this.health = healthPoints;
     }
+
+    //getters
+    public String getAnimalName(){
+        return name;
+    }
+    public String getAnimalGender(){ return gender; }
+    public int getAnimalHealth(){
+        return health;
+    }
+
 
     //SKa metoden vara här eller i subklassen?
     public void die(){
+
+    }
+
+    public void feedAnimals(){
 
     }
 }
