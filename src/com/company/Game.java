c1719fb17c464f731fdec4df4405b259a6e8b854
snabelA00 <ja000411@gmail.com>
2020-10-19 18:29:15 +0200

    - Personal information is now updated in terminal every round, who's turn it is and the player's account balance
    - Displays a list of current owned animals and food items in terminal
    
    - Player can select different actions which are represented in terminal with numbers.

--- src/com/company/Game.java
+++ src/com/company/Game.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 public class Game {
 
     Scanner scanner = new Scanner(System.in);
-    private ArrayList<String> playerNames = new ArrayList<>();
+
 
     public Game(){
         print("\n".repeat(1));
@@ -21,10 +21,9 @@ public class Game {
         print("- Write the name of all the players");
         for(var i = 1; i <= numberOfPlayers; i++) {
             print("- Spelare " + (i) + ":s namn: ");
-            playerNames.add(scanner.nextLine());
+            String inputPlayerName = scanner.nextLine();
+            new Player(inputPlayerName, 50000);
         }
-
-        print(playerNames.toString());
 
         print("- How many rounds would you like to play? Choose between 5 - 30:");
         int xRounds = Integer.valueOf(scanner.nextLine());
@@ -37,16 +36,41 @@ public class Game {
     public void main(int numberOfRounds){
 
         for(int i = 1; i <= numberOfRounds; i++){
-            print("- Lets begin! What's your next move? (write a number)");
-            print(" 1. Buy an Animal\n 2. Sell an Animal\n 3. Buy Food\n 4. Feed animals");
-            int action = Integer.valueOf(scanner.nextLine());
-            switch (action){
-                case 1 -> new Store().buyAnimals();
-                case 2 -> new Store().sellAnimals();
-                case 3 -> new Store().buyFood();
-                case 4 -> new Animal("horsy", "female").feedAnimals();
+            for(var player: Player.allPlayers) {
+                // Display player information
+                int index = Player.allPlayers.indexOf(player);
+                var currentPlayer = Player.allPlayers.get(index);
+                print("- " + currentPlayer.getPlayerName() + "'s turn");
+                print("Balance: " + currentPlayer.getAccountBalance() + "€");
+
+                // Animal list
+                System.out.println("You own " + currentPlayer.allAnimals.size() + " animals");
+
+                for(int j = 0; j < currentPlayer.allAnimals.size(); j++ ) {
+                    print(j+1 + ". " + currentPlayer.allAnimals.get(j).getClass().getSimpleName()
+                            + ", " + currentPlayer.allAnimals.get(j).getAnimalName()
+                            + ", " + currentPlayer.allAnimals.get(j).getAnimalGender()
+                            + ", " + currentPlayer.allAnimals.get(j).getAnimalHealth() + "hp"
+                    );
+                }
+
+                System.out.println("\nFood storage: ");
+                for (int j = 0; j < currentPlayer.allFood.size(); j++) {
+                    print(j + 1 + ". " + currentPlayer.allFood.get(j).getClass().getSimpleName());
+                    // print("- Hay = " + 0 + "\n- Carrots = " + 0 + "\n- Seed = " + 0);
+                }
+
+                print("- Lets begin! What's your next move? (write a number)");
+                print(" 1. Buy an Animal\n 2. Sell an Animal\n 3. Buy Food\n 4. Feed animals");
+                int action = Integer.valueOf(scanner.nextLine());
+                switch (action) {
+                    case 1 -> new Store().buyAnimals(currentPlayer);
+                    case 2 -> new Store().sellAnimals();
+                    case 3 -> new Store().buyFood(currentPlayer);
+                    //case 4 -> new Animal("horsy", "female").feedAnimals();
+                }
+                print("End of turn" + "\n".repeat(50));
             }
-            print("End of turn");
         }
 
     }
