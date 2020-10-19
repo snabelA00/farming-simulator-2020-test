66c46c4ea9d694633f44d72ebce008401812d506
snabelA00 <ja000411@gmail.com>
2020-10-19 18:46:55 +0200

    Player constructor: - Initialized Player objects with name, account balance, Arraylist<Food>, Arraylist<Animal> and added to Arraylist<Player>
    added getters and setters

--- src/com/company/Player.java
+++ src/com/company/Player.java
@@ -1,11 +1,58 @@
 package com.company;
 
+import java.util.ArrayList;
+
+
 public class Player {
 
-    private String name;
-    private int accountBalance = 50000;
+    public static ArrayList<Player> allPlayers = new ArrayList<Player>();
 
-    public Player(String initialName){
+    private String name;
+    private int accountBalance;
+    ArrayList<Animal> allAnimals;
+    ArrayList<Food> allFood;
+
+
+    public Player(String initialName, int money){
         this.name = initialName;
+        this.accountBalance = money;
+        this.allFood = new ArrayList<Food>();
+        this.allAnimals = new ArrayList<Animal>();
+
+        Player.allPlayers.add(this);
     }
+
+    public void addAnimal(Animal animal){
+        this.allAnimals.add(animal);
+    }
+
+    public void addFood(Food food){
+        this.allFood.add(food);
+    }
+
+    public String getPlayerName(){
+        return name;
+    }
+
+    public int getAccountBalance(){
+        return accountBalance;
+    }
+
+    //setters
+    public void setAccountBalance(int x){
+        this.accountBalance = x;
+    }
+
+    /*
+    public String getAnimals(){
+        return allAnimals;
+    }
+
+    public String getFood(){
+        return allFood;
+    }
+
+     */
+
+
 }
