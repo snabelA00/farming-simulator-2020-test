c7b0edefa8537f029b7967a8d1cf821fe8ad799e
snabelA00 <ja000411@gmail.com>
2020-10-20 12:15:03 +0200

    Class Animal - added static method reduceHealth() which loops through the players list of animals and reduces its health with a random generated number between 10-30.
    - added static method checkAnimalHealth() which determines if any of the animals health equals 0 or below. If it's true the player gets informed and the item is removed from the list.

--- src/com/company/Animal.java
+++ src/com/company/Animal.java
@@ -1,11 +1,11 @@
 package com.company;
 
-public class Animal {
+public abstract class Animal {
 
     protected String name;
     protected String gender;
     protected int health;
-
+    public abstract int getAnimalPrice();
 
     public Animal(String name, String gender, int healthPoints){
         this.name = name;
@@ -21,14 +21,28 @@ public class Animal {
     public int getAnimalHealth(){
         return health;
     }
+    //setters
+    public void setAnimalHealth(int x){ this.health = x;}
 
-
-    //SKa metoden vara här eller i subklassen?
-    public void die(){
-
+    public static void reduceHealth(Player currentPLayer) {
+        for (var eachAnimal : currentPLayer.allAnimals) {
+            //Math.random()*(maximum - minimum) + minimum
+            int reduceHp = (int) (Math.random() * (50 - 40)) + 40;
+            var index = currentPLayer.allAnimals.indexOf(eachAnimal);
+            var getHp = currentPLayer.allAnimals.get(index).getAnimalHealth();
+            var setHp = getHp - reduceHp;
+            currentPLayer.allAnimals.get(index).setAnimalHealth(setHp);
+        }
     }
 
-    public void feedAnimals(){
-
+    public static void checkAnimalHealth(Player currentPlayer){
+        for (int i = 0; i < currentPlayer.allAnimals.size(); i++) {
+            if (currentPlayer.allAnimals.get(i).getAnimalHealth() <= 0) {
+                System.out.println(("- Your " + currentPlayer.allAnimals.get(i).getClass().getSimpleName().toLowerCase()
+                        + " " + currentPlayer.allAnimals.get(i).getAnimalName() + " died..."));
+                currentPlayer.allAnimals.remove(i);
+            }
+        }
     }
+
 }
